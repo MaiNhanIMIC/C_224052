de3c4c4b57e1fb48d3f6e80a90efbe2b028cbcdd
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-09 10:34:30 +0700

    thêm thư viện toan.h, toan.c

--- C_224052/main.c
+++ C_224052/main.c
@@ -1,12 +1,15 @@
 ﻿#include <stdio.h>
-int Cong(int a, int b);
-
+#include "toan.h"
 
 void main()
 {
-	int x = Cong(1, 2);
-	printf("Hello world, x: %d \n", x);
-	
+	int x = 1;
+	int y = 2;
+
+	printf("Cong: %d\n", cong(x, y));
+	printf("Tru: %d\n", tru(x, y));
+	printf("Nhan: %d\n", nhan(x, y));
+	printf("Chia: %f\n", chia(x, y));
 }
 
 
--- C_224052/toan.c
+++ C_224052/toan.c
@@ -6,9 +6,53 @@ Input:
 Output:
 	int: kết quả của phép tính
 */
-int Cong(int a, int b)
+int cong(int a, int b)
 {
 	int ketqua = 0;
 	ketqua = a + b;
+	return ketqua;
+}
+/*
+Chức năng: tru 2 số nguyên
+Input:
+	int a: số nguyên thứ nhất
+	int b: số nguyên thứ hai
+Output:
+	int: kết quả của phép tính
+*/
+int tru (int a, int b)
+{
+	int ketqua = 0;
+	ketqua = a - b;
+	return ketqua;
+	
+}
+/*
+Chức năng: nhan 2 số nguyên
+Input:
+	int a: số nguyên thứ nhất
+	int b: số nguyên thứ hai
+Output:
+	int: kết quả của phép tính
+*/
+int nhan(int a, int b)
+{
+	int ketqua = 0;
+	ketqua = a * b;
+	return ketqua;
+}
+
+/*
+Chức năng: chia 2 số nguyên
+Input:
+	int a: số nguyên thứ nhất
+	int b: số nguyên thứ hai
+Output:
+	float: kết quả của phép tính
+*/
+float chia(int a, int b)
+{
+	float ketqua = 0;
+	ketqua = (float)a / b;
 	return ketqua;
 }

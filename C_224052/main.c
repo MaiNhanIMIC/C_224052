185b589c1646ca4971cfc1ea736f74d8a1a973fa
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-09 11:45:48 +0700

    Hàm có input là mảng

--- C_224052/main.c
+++ C_224052/main.c
@@ -1,15 +1,38 @@
 ﻿#include <stdio.h>
 #include "toan.h"
 
+/*
+	chức năng: tìm giá trị lớn nhất trong mảng
+	input:
+		mảng:
+			+ địa chỉ bất đầu của mảng
+			+ số lượng phần tử trong mảng
+	output:
+		int: số lớn nhất
+*/
+int Tim_Gia_Tri_Lon_Nhat(int* arr, int so_luong)
+{
+	int max = 0;
+	for (int i = 0; i < so_luong; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+
+	return max;
+}
+
 void main()
 {
-	int x = 1;
-	int y = 2;
+	//KHAI BÁO array bao gồm 5 đối tượng int
+	int mang[5] = { 1,3000,65,3,2 };
+	int lon_nhat = 0;
+	// in ra giá trị lớn nhất trong mang
+	lon_nhat = Tim_Gia_Tri_Lon_Nhat(mang, 5);
 
-	printf("Cong: %d\n", cong(x, y));
-	printf("Tru: %d\n", tru(x, y));
-	printf("Nhan: %d\n", nhan(x, y));
-	printf("Chia: %f\n", chia(x, y));
+	printf("gia tri lon nhat: %d \n", lon_nhat);
 }
 
 

639bd3834f445809bde77d3d1365ae3b0d21cec7
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-03-23 09:23:41 +0700

    chuổi

--- C_224052/main.c
+++ C_224052/main.c
@@ -1,38 +1,59 @@
 ﻿#include <stdio.h>
-#include "toan.h"
 
-/*
-	chức năng: tìm giá trị lớn nhất trong mảng
-	input:
-		mảng:
-			+ địa chỉ bất đầu của mảng
-			+ số lượng phần tử trong mảng
-	output:
-		int: số lớn nhất
-*/
-int Tim_Gia_Tri_Lon_Nhat(int* arr, int so_luong)
+int SoLuongKyTu(char* str)
 {
-	int max = 0;
-	for (int i = 0; i < so_luong; i++)
+	int sl = 0;
+	while (str[sl] != 0)
 	{
-		if (max < arr[i])
-		{
-			max = arr[i];
-		}
+		sl++;
 	}
-
-	return max;
+	return sl;
 }
+
+// Chức nâng: dùng để tìm ký tự c trong chuỗi str
+// Input:
+//	char* chuoi - chuổi cần tìm
+//	char c - ký tự cần tìm trong chuổi str
+// Output: (char*) địa chỉ của ký tự c trong chuổi str
+char* TimKyTu(char* chuoi, char ky_tu)
+{
+	int chieu_dai_chuoi = SoLuongKyTu(chuoi);
+	for (int i = 0; i < chieu_dai_chuoi; i++)
+	{
+		if (chuoi[i] == ky_tu)
+			return chuoi + i;
+	}
+	return 0;
+}
+
+// Chức nâng: dùng để tìm chuỗi subStr trong chuỗi str
+// Input:
+//	char* str - chuỗi cần tìm
+//	char* sub_str - chuỗi từ khóa cần tìm
+// Output: (char*) địa chỉ của chuổi sub_str trong str
+char* TimChuoi(char* str, char* sub_str)
+{
+	int chieu_dai_str = SoLuongKyTu(str);
+	int chieu_dai_sub_str = SoLuongKyTu(sub_str);
+	for (int i = 0; i < chieu_dai_str; i++)
+	{
+		int j = 0;
+		for (j = 0; j < chieu_dai_sub_str; j++)
+		{
+			if (str[i + j] != sub_str[j])
+				break;
+		}
+		if (j == chieu_dai_sub_str)
+			return str + i;
+	}
+	return 0;
+}
+
 
 void main()
 {
-	//KHAI BÁO array bao gồm 5 đối tượng int
-	int mang[5] = { 1,3000,65,3,2 };
-	int lon_nhat = 0;
-	// in ra giá trị lớn nhất trong mang
-	lon_nhat = Tim_Gia_Tri_Lon_Nhat(mang, 5);
+	char str[] = "xin chao moi nguoi";
+	char sub_str[] = "chau";
 
-	printf("gia tri lon nhat: %d \n", lon_nhat);
+	char* x = TimChuoi(str, sub_str);
 }
-
-

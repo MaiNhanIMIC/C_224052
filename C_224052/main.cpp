56c9f5ee3642c6023bfbfccac7fb7674e3c8a9a4
MaiNhanIMIC <mainhan.imic@gmail.com>
2025-04-20 11:30:08 +0700

    Cpp

---
+++ C_224052/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <malloc.h>
+#include <stdio.h>
+#include "hinh_hoc.h"
+
+hinh_chu_nhat_c C;
+
+int main()
+{
+	hinh_chu_nhat_c A;
+	hinh_chu_nhat_c B(1,2);
+	A.tinhChuVi();
+
+	return 0;
+}
